46eb65a6efe5ec87a29e9fcc98af5e975b127cf5
Ryan Basnet <ryandbasnet@gmail.com>
2018-02-25 00:36:46 +1100

    add nancy web service project

---
+++ api.webserv/modules/HomeModule.cs
@@ -0,0 +1,18 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace api.webserv.modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            System.Diagnostics.Debug.WriteLine("Its to debug a system");
+            Get["/"] = p => "Hello from plural sight";
+
+        }
+    }
+}
